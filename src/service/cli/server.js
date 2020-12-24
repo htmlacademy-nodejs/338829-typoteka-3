8c8c9fa3bf619f24369e13e99ad92bb8a650802f
K. Mezhuritskiy <resetus@gmail.com>
2020-12-25 00:00:32 +0300

    Рефакторинг hhtp -> express api

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,11 +1,10 @@
 'use strict';
 
 const chalk = require(`chalk`);
-const http = require(`http`);
-const fs = require(`fs`).promises;
+const express = require(`express`);
+const postRouter = require(`../routes/posts`);
 
 const {
-  FILE_MOCK_PATH,
   DEFAULT_PORT,
   CliCommand,
   HttpCode
@@ -14,59 +13,21 @@ const {
 const {checkNumParam} = require(`../../utils`);
 
 const startHttpServer = (port) => {
-  http.createServer(onClientConnect)
-    .listen(port)
-    .on(`listening`, (err) => {
-      if (err) {
-        return console.info(chalk.red(`[http]: Ошибка при создании сервера`, err));
-      }
+  const app = express();
+  app.use(express.json());
+  app.use(`/posts`, postRouter);
 
-      return console.info(chalk.green(`[http]: Ожидаю соединений на ${port}`));
-    });
-};
-
-const onClientConnect = async (req, res) => {
-  const sendNotFound = () => {
-    sendResponse(res, HttpCode.NOT_FOUND, `<h1>Not found</h1>`);
-  };
-
-  switch (req.url) {
-    case `/`:
-      try {
-        const content = await fs.readFile(FILE_MOCK_PATH, `utf-8`);
-        const mocks = JSON.parse(content);
-
-        const template = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-        sendResponse(res, HttpCode.OK, `<ul>${template}</ul>`);
-      } catch (error) {
-        console.info(chalk.red(error));
-        sendNotFound();
-      }
-      break;
-
-    default:
-      sendNotFound();
-      break;
-  }
-};
-
-const sendResponse = (res, statusCode, template) => {
-  const html = (`
-    <!Doctype html>
-      <html lang="ru">
-      <head>
-        <title>Node Http Server</title>
-      </head>
-      <body>${template}</body>
-    </html>
-  `).trim();
-
-  res.statusCode = statusCode;
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
+  app.use((req, res) => {
+    res.status(HttpCode.NOT_FOUND).send(`Not found`);
   });
 
-  res.end(html);
+  app.use((err, req, res, _next) => {
+    res.status(HttpCode.INTERNAL_SERVER_ERROR).send(`Server Error`);
+  });
+
+  app.listen(port, () => {
+    console.info(chalk.green(`[Api]: Ожидаю соединений на ${port}`));
+  });
 };
 
 module.exports = {
---
+++ src/service/routes/posts.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const fs = require(`fs/promises`);
+const {Router} = require(`express`);
+const {FILE_MOCK_PATH} = require(`../../constans`);
+
+const postRouter = new Router();
+postRouter.get(`/`, async (req, res) => {
+  try {
+    const fileContent = await fs.readFile(FILE_MOCK_PATH, `utf-8`);
+    const mocks = JSON.parse(fileContent);
+    res.json(mocks);
+  } catch (error) {
+    res.json([]);
+  }
+});
+
+module.exports = postRouter;
